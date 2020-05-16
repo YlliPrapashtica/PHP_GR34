dee67e8b0095589043c6e50d3fd8f7dfedc44a4e
ArgjentaGashi <58112102+ArgjentaGashi@users.noreply.github.com>
2020-05-16 22:51:39 +0200

    error handling

--- connect.php
+++ connect.php
@@ -1,13 +1,31 @@
 <?php
 
-$dbhost = 'localhost';
+$dbhost = 'localhost:8111';
 $username = 'root';
-$password = 'root';
+$password = '';
 $db = 'hotel_database';
+try {
+    if( $con = mysqli_connect($dbhost,$username,$password,$db))
+           {
+            $mydb = mysqli_select_db($con,$db);
+        echo "Connected!";
+        if($mydb)
+        {
+            echo "Database was found!";
+        }
+        else
+        {
+            echo "Error in connection";
+        }
+           }
+    else
+        {
+        throw new Exception('Unable to connect');
+    }
 
-$con = mysqli_connect($dbhost,$username,$password,$db);
-
-$mydb = mysqli_select_db($con,$db);
-
-
-?>
+    }
+  catch(Exception $e)
+{
+    echo $e->getMessage();
+}
+?> 
