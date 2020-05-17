48ba93cbdc073edfbd274627482cc903f31e0b80
rgnpcrz <48785434+rgnpcrz@users.noreply.github.com>
2020-05-17 19:38:09 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -21,59 +21,9 @@
 	
 	<!-- Header -->
 
-	<header class="header">
-		<div class="header_content d-flex flex-column align-items-center justify-content-lg-end justify-content-center">
-			
-			<!-- Logo -->
-			<div class="logo"><a href="#"><img class="logo_1" src="images/logo.png" alt=""><img class="logo_2" src="images/logo_2.png" alt=""><img class="logo_3" src="images/logo_3.png" alt=""></a></div>
-
-			<!-- Main Nav -->
-			<nav class="main_nav">
-				<ul class="d-flex flex-row align-items-center justify-content-start">
-					<li><a href="index.html">Home</a></li>
-					<li><a href="about.html">About us</a></li>
-					<li><a href="rooms.html">Rooms</a></li>
-					<li class="active"><a href="contact.html">Contact</a></li>
-					<li><a href="Log in.html">My Account</a></li>
-					<li><a href="booking.html">Book You Room</a></li>
-				</ul>
-			</nav>
-
-		
-
-			<!-- Header Right -->
-			<div class="header_right d-flex flex-row align-items-center justify-content-start">
-				
-				<!-- Search Activation Button -->
-				<div class="search_button">
-					<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 512 512" enable-background="new 0 0 512 512" width="512px" height="512px">
-						<g>
-							<path d="M495,466.2L377.2,348.4c29.2-35.6,46.8-81.2,46.8-130.9C424,103.5,331.5,11,217.5,11C103.4,11,11,103.5,11,217.5   S103.4,424,217.5,424c49.7,0,95.2-17.5,130.8-46.7L466.1,495c8,8,20.9,8,28.9,0C503,487.1,503,474.1,495,466.2z M217.5,382.9   C126.2,382.9,52,308.7,52,217.5S126.2,52,217.5,52C308.7,52,383,126.3,383,217.5S308.7,382.9,217.5,382.9z" fill="#FFFFFF"></path>
-						</g>
-					</svg>
-				</div>
-
-				<!-- Header Link -->
-				<div class="header_link"><a href="#">Book Your Room Now</a></div>
-
-				<!-- Hamburger Button -->
-				<div class="hamburger"><i class="fa fa-bars" aria-hidden="true"></i></div>
-
-			</div>
-
-			<!-- Search Panel -->
-			<div class="search_panel">
-				<div class="search_panel_content d-flex flex-row align-items-center justify-content-start">
-					<img src="images/search.png" alt="">
-					<form action="#" class="search_form">
-						<input type="text" class="search_input" placeholder="Type your search here" required="required">
-					</form>
-					<div class="search_close ml-auto d-flex flex-column align-items-center justify-content-center"><div></div></div>
-				</div>
-			</div>
-		</div>
-			
-	</header>
+	<?php
+		include('header.php')
+	?>
 
 	<!-- Logo Overlay -->
 
@@ -192,20 +142,18 @@
 							<p>Maecenas sollicitudin tincidunt maximus. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices in quis augue. Donec imperd iet leo eget tortor dictum, eget varius eros sagittis.</p>
 						</div>
 						<div class="contact_form_container">
-							<form action="contactform.php" method="POST"class="contact_form text-center">
-							
+							<form action="#" class="contact_form text-center">
 								<div class="row">
 									<div class="col-lg-6">
-										<input type="text" name="name" class="contact_input" placeholder="Your name" required="required">
+										<input type="text" class="contact_input" placeholder="Your name" required="required">
 									</div>
 									<div class="col-lg-6">
-										<input type="email" name="email" class="contact_input" placeholder="Your email" required="required">
+										<input type="email" class="contact_input" placeholder="Your email" required="required">
 									</div>
 								</div>
-								<input type="text" name="subject" class="contact_input" placeholder="Subject">
-								<textarea class="contact_input" name="message" placeholder="Message" required="required"></textarea>
+								<input type="text" class="contact_input" placeholder="Subject">
+								<textarea class="contact_input" placeholder="Message" required="required"></textarea>
 								<button class="contact_button">send message</button>
-								
 							</form>
 						</div>
 					</div>
